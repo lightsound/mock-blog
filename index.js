f76aa55c8d661de947acf54592e3ec1f7338ff6c
skoto <0120koto@gmail.com>
2020-12-24 21:54:09 +0900

    🚧

--- index.js
+++ index.js
@@ -3,17 +3,23 @@ const main = document.querySelector("main");
 async function getPosts() {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
   const posts = await res.json();
-  console.log(posts);
+  return posts;
 }
 
-window.addEventListener("load", () => {
-  getPosts();
+function createArticle(post) {
   const article = document.createElement("article");
   const title = document.createElement("h1");
   const body = document.createElement("p");
-  title.textContent = "タイトルですよ！";
-  body.textContent = "本文ですよ！";
+  title.textContent = post.title;
+  body.textContent = post.body;
   article.appendChild(title);
   article.appendChild(body);
   main.appendChild(article);
+}
+
+window.addEventListener("load", async () => {
+  const posts = await getPosts();
+  posts.forEach((post) => {
+    createArticle(post);
+  });
 });
