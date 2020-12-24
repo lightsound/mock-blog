e3a720a46448ab1f3b94b24249ffe010bb78e265
skoto <0120koto@gmail.com>
2020-12-24 22:21:18 +0900

    🚧

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const main = document.querySelector("main");
+const input = document.querySelector("input");
+const textarea = document.querySelector("textarea");
+const button = document.querySelector("button");
 
 async function getPosts() {
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
@@ -14,12 +17,44 @@ function createArticle(post) {
   body.textContent = post.body;
   article.appendChild(title);
   article.appendChild(body);
-  main.appendChild(article);
+  return article;
 }
 
 window.addEventListener("load", async () => {
   const posts = await getPosts();
   posts.forEach((post) => {
-    createArticle(post);
+    const article = createArticle(post);
+    main.appendChild(article);
   });
+});
+
+button.addEventListener("click", async () => {
+  const title = input.value;
+  const body = textarea.value;
+
+  if (!title.trim() || !body.trim()) {
+    alert("入力必須です！");
+    return;
+  }
+
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify({
+        title: title,
+        body: body,
+        userId: 1,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+    const data = await res.json();
+    const article = createArticle(data);
+    main.prepend(article);
+    input.value = "";
+    textarea.value = "";
+  } catch (error) {
+    alert("失敗！");
+  }
 });
